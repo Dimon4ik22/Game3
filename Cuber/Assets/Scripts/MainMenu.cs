4dc6ed85afa02b3c0961d3b4597b90397c239852
Дмитрий Фильков <dima.filkov01@mail.ru>
2021-02-22 21:08:17 +0300

    Update UI

--- Cuber/Assets/Scripts/MainMenu.cs
+++ Cuber/Assets/Scripts/MainMenu.cs
@@ -2,11 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
     public void PlayTutorial()
     {
         SceneManager.LoadScene(1);
+    }
+    public void PlayLevel1()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void PlayLevel2()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void PlayLevelE()
+    {
+        SceneManager.LoadScene(4);
     }
 }
