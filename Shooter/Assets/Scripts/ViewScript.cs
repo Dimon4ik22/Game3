bd3cf7887602f5a73bc43abf8daecf0f0dd85051
Дмитрий Фильков <dima.filkov01@mail.ru>
2021-03-05 19:47:02 +0300

    Shooter
    Добавил новый проект

---
+++ Shooter/Assets/Scripts/ViewScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewScript : MonoBehaviour
+{
+    public float mouseSensitivity = 1000f;
+    public Transform playerBody;
+    float xRot = 0f;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRot -= mouseY;
+        Mathf.Clamp(mouseY, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRot, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
